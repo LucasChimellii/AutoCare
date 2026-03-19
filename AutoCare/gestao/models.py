861eca8be89f48527a7a1bfdc464f068630c491b
Lucas Vinicius <lvchimelli@gmail.com>
2026-03-19 13:36:17 -0300

    feat: moderniza interface de busca e blinda cadastro de veículos
    - Aplica Bootstrap 5 (Cards, Badges e Alerts) para interface responsiva.
    - Cria filtro na View para ignorar traços e espaços na busca do usuário.
    - Sobrescreve método save() no Model para padronizar placas (8  dígitos, maiúsculo).
    - Adiciona travas de segurança (maxlength e oninput) direto no HTML.

--- AutoCare/gestao/models.py
+++ AutoCare/gestao/models.py
@@ -8,29 +8,38 @@ class Cliente(models.Model):
     def __str__(self):
         return self.nome
 
-
-
 class Carro(models.Model):
     cliente = models.ForeignKey (Cliente, on_delete=models.CASCADE)
-    placa = models.CharField(max_length = 10)
+    placa = models.CharField(max_length = 8)
     modelo = models.CharField(max_length = 100)
     marca = models.CharField(max_length = 50, null=True, blank=True)
     ano = models.IntegerField()
 
+    def save(self, *args, **kwargs):
+        # Converte automaticamente quando cadastrado no banco de dados para um formato de placa geral 
+        if self.placa:
+            self.placa = self.placa.replace('-', '').replace(' ', '').strip().upper()
+            
+        # E Salva no banco de dados
+        super().save(*args, **kwargs)
+
+
     def __str__(self):
-        return (f'{self.placa} - {self.modelo}')
+        return (f'{self.marca} - {self.modelo}')
     
 class Servico(models.Model):
     carro = models.ForeignKey (Carro, on_delete=models.CASCADE)
     status_servico = models.CharField(max_length = 100, default= 'Na Fila')
     valor = models.DecimalField(max_digits = 8, decimal_places =2)
+    descricao = models.CharField(max_length=200, null=True, blank= True)
     data_entrada = models.DateTimeField(auto_now_add=True)
     data_saida = models.DateTimeField(null=True, blank=True)
-    
+
     def __str__(self):
-        return (f'{self.carro}, {self.status_servico}, {self.data_entrada}')
+        return (f'{self.carro}, {self.carro.cliente}, {self.carro.placa}, {self.status_servico}, {self.data_entrada}')
 
 class FotoAvaria(models.Model):
     servico = models.ForeignKey(Servico, on_delete=models.CASCADE)
     imagem = models.ImageField(upload_to='avarias/')
     descricao = models.CharField(max_length = 100)
+
--- AutoCare/gestao/views.py
+++ AutoCare/gestao/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Servico
+def pagina_inicial(request):
+    placa_digitada = request.GET.get('placa')
 
-# Create your views here.
+    placa_formatada = placa_digitada.replace('-', '').upper() # Garante que a placa digitada seja formatada para busca no banco de dados
+    servico_encontrado = None
+
+    if placa_digitada:
+        servico_encontrado = Servico.objects.filter(carro__placa__iexact= placa_formatada).first
+        
+    contexto = {
+        'servico': servico_encontrado,
+        'placa_buscada': placa_formatada,
+    }
+
+    return render(request, 'gestao/home.html', contexto)
